52bb602cb43b5361b51e19b54bf2524837357a87
fiqrianwar <fiqrianwar01@gmail.com>
2024-12-12 16:48:55 +0800

    setup database postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
